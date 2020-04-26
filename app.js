fbcc9cc01095c244757e4869c4f240247d203912
skari <skari@deakin.edu.au>
2020-04-26 16:10:01 +1000

    Initialised project

---
+++ app.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+
+//Port
+const port = process.env.PORT || 8000;
+
+//Starting Server
+app.listen(port, () => {
+  console.log(`app is running at ${port}`);
+});
