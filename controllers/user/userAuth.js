bb6c50a31c034629c15d35915af3fa8bbb40ec5f
skari <skari@deakin.edu.au>
2020-04-29 13:12:15 +1000

    Authentication - SignIn & SIgnOut routes

--- controllers/user/userAuth.js
+++ controllers/user/userAuth.js
@@ -3,6 +3,65 @@ const { check, validationResult } = require("express-validator");
 var jwt = require("jsonwebtoken");
 var expressJwt = require("express-jwt");
 
+exports.signup = (req, res) => {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.status(422).json({
+      error: errors.array()[0].msg,
+    });
+  }
+
+  //Saving user to DB
+  const user = new User(req.body);
+  user.save((err, user) => {
+    if (err) {
+      return res.status(400).json({
+        err: "NOT able to save user in DB",
+      });
+    }
+    res.json({
+      name: user.name,
+      email: user.email,
+      id: user._id,
+    });
+  });
+};
+
+exports.signin = (req, res) => {
+  const errors = validationResult(req);
+  const { email, password } = req.body;
+
+  if (!errors.isEmpty()) {
+    return res.status(422).json({
+      error: errors.array()[0].msg,
+    });
+  }
+
+  User.findOne({ email }, (err, user) => {
+    if (err || !user) {
+      return res.status(400).json({
+        error: "User email does not exist",
+      });
+    }
+
+    if (!user.authenticate(password)) {
+      return res.status(401).json({
+        error: " Email & password do not match",
+      });
+    }
+
+    //Create token
+    const token = jwt.sign({ _id: user._id }, process.env.SECRET);
+    //Put token in cookie
+    res.cookie("token", token, { expire: new Date() + 9999 });
+
+    //send response to frontend
+    const { _id, name, email, role } = user;
+    return res.json({ token, user: { _id, name, email, role } });
+  });
+};
+
 exports.signout = (req, res) => {
   res.clearCookie("token");
   res.json({
--- routes/user/userAuth.js
+++ routes/user/userAuth.js
@@ -9,6 +9,33 @@ const {
   isSignedIn,
 } = require("../../controllers/user/userAuth");
 
+router.post(
+  "/signup",
+  //using express validator to validate in routes
+  [
+    check("name")
+      .isLength({ min: 3 })
+      .withMessage("name should be at least 3 charecters"),
+    check("email").isEmail().withMessage("email is required"),
+    check("password")
+      .isLength({ min: 3 })
+      .withMessage("password should be atleast 3 charecters"),
+  ],
+  signup
+);
+
+router.post(
+  "/signin",
+  //using express validator to validate in routes
+  [
+    check("email").isEmail().withMessage("email is required"),
+    check("password")
+      .isLength({ min: 1 })
+      .withMessage("password sfield is required"),
+  ],
+  signin
+);
+
 router.get("/signout", signout);
 
 module.exports = router;
